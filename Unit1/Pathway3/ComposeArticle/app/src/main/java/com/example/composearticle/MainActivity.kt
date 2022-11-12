c53f61f6f815a3c08c04042043e2690b495c2f91
ryucha <qtholyday@gmail.com>
2022-11-12 21:58:51 +0900

    데이터 넣기

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composearticle
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,12 +25,30 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeArticleApp()
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+    val title: String = stringResource(id = R.string.title_jetpack_compose_tutorial)
+    val shortDescription: String = stringResource(id = R.string.compose_short_desc)
+    val longDescription: String = stringResource(id = R.string.compose_long_desc)
+    val imagePainter: Painter = painterResource(id = R.drawable.bg_compose_background)
+    
+    ArticleCard(
+        title = title,
+        shortDescription = shortDescription,
+        longDescription = longDescription,
+        imagePainter = imagePainter
+    )
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +58,23 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column() {
+        Image(
+            painter = imagePainter,
+            contentDescription = null,
+        )
+        Text(text = title)
+        Text(shortDescription)
+        Text(longDescription)
+
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeArticleTheme {
+        ComposeArticleApp()
+    }
+}
